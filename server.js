152de1f7dd39c2845d252967be2d283dcc791cd6
toursoinvestimentos-web <toursoinvestimentos@gmail.com>
2026-04-16 12:49:22 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/preco", (req, res) => {
   res.json({
